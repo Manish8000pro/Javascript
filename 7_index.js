a914310e0df69d58964ccfd2611f027772e65fd7
Manish8000pro <manishkumawat6227@gmail.com>
2025-05-29 11:50:31 +0530

    New updates

---
+++ 7_index.js
@@ -0,0 +1,19 @@
+let arr = [1,2,3,4,5,7]
+// Index=  0,1,2,3,4,5
+arr[0] = 5421
+
+// console.log(arr)
+// console.log(arr.length)
+
+// console.log(arr,typeof arr)
+// console.log(arr[0])
+// console.log(arr[2])
+// console.log(arr[3])
+
+// console.log(arr.toLocaleString())
+// console.log(arr.join(" and "))
+
+let a1 = [1,2,3]
+let a2 = [4,5,6]
+let a3 = [9,8,7]
+console.log(a1.concat(a3))
