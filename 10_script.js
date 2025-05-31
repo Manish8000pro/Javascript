5315640070ee5050af07532d3d2fe1755a4cd2b8
Manish8000pro <manishkumawat6227@gmail.com>
2025-05-31 12:23:52 +0530

    level up in dom

---
+++ 10_script.js
@@ -0,0 +1,8 @@
+console.log("Manish")
+
+let boxes = document.getElementsByClassName("box")
+    console.log(boxes)
+
+    boxes[2].style.backgroundcolor = "red"
+
+// document.getElementById("redbox").style.backgroundcolor = "red"
