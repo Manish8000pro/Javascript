a24b42eb70ba502fa11d4f1e498efb12586c3a04
Manish8000pro <manishkumawat6227@gmail.com>
2025-06-02 11:46:56 +0530

    prob.

--- 10_script.js
+++ 10_script.js
@@ -1,9 +1,9 @@
 console.log("Manish")
 
-let boxes = document.getElementsByClassName("box")
+let boxes = document.getElementsByClassName(".box")
     console.log(boxes)
 
-    boxes[2].style.backgroundcolor = "red"
+    boxes(2).style.backgroundcolor = "red"
 
 // document.getElementById("redbox").style.backgroundcolor = "red"
 
