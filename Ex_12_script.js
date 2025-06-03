98c4f76fddf2a9b2679b89d88ef4b3786ca16ea6
Manish8000pro <manishkumawat6227@gmail.com>
2025-06-03 17:28:43 +0530

    excercise.11

--- Ex_12_script.js
+++ Ex_12_script.js
@@ -1,15 +1,15 @@
 console.log("Ex_12_script.js initializing")
-// let Box = document.querySelector("Box")
+// let boxes = document.getElelmentByclassName("box")
 let boxes = document.querySelector(".container").children
 
 function getrandomcolor(){
-    let val1 = Math.random();
-    let val2 = 13;
-    let val3 = 15;
+    let val1 = Math.ceil(0 + Math.random()* 255);
+    let val2 = Math.ceil(0 + Math.random()* 255);
+    let val3 = Math.ceil(0 + Math.random()* 255);
     return `rgb(${val1}, ${val2}, ${val3})`
 }
 
 Array.from(boxes).forEach(e=>{
-    // e.style.backgroundcolor = getrandomcolor()
     e.style.color = getrandomcolor()
+    e.style.background.color = getrandomcolor()
 })
