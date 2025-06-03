de47172b4a4cb56c661405a6199fe47a1cdbf934
Manish8000pro <manishkumawat6227@gmail.com>
2025-06-03 17:26:29 +0530

    update

--- 10_script.js
+++ 10_script.js
@@ -5,12 +5,12 @@ console.log("Manish")
 
 //     boxes(2).style.backgroundcolor = "red"
 
-// document.getElementById("redbox").style.backgroundcolor = "red"
+document.getElementById("redbox").style.backgroundcolor = "red"
 
-// document.querySelector(".box").style.backgroundcolor = "green";
-// console.log(document.querySelectorAll(".box"))
+document.querySelector(".box").style.backgroundcolor = "green";
+console.log(document.querySelectorAll(".box"))
 
-// document.querySelectorAll(".box").forEach(e=>{
+document.querySelectorAll(".box").forEach(e=>{
 
-//     e.style.backgroundcolor = "green"
-// })
+    e.style.backgroundcolor = "green"
+})
