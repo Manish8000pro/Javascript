1cf82f18ba6599e10a29557a4ae692390ece3930
Manish8000pro <manishkumawat6227@gmail.com>
2025-06-05 13:49:17 +0530

    looking

--- 12_script.js
+++ 12_script.js
@@ -1,17 +1,3 @@
-// let button = document.getElementById("button")
-
-// button.addEventListener("dblclick", () =>{
-//     document.querySelector(".box").innerHTML = "<b>yayy you are clicked</b> enjoy your click!"
-// })
-
-// button.addEventListener("contextmenu", () =>{
-//     alert("Don't hack us by right click please")
-// })
-
-// document.addEventListener("keydown", () =>{
-//     console.log(e,e.key,e.keyCode)
-// })
-
 let button = document.getElementById("button")
 // List of all mouse events
 // https://developer.mozilla.org/en-US/docs/Web/API/Element#mouse_events
